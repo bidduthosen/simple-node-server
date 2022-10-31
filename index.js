7a01f484c25b2b7951dea7621abc0e018d8c95fa
bidduthosen <mdbidduthosen34@gmail.com>
2022-10-31 18:35:53 +0600

    done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000 ;
 
+
+app.use(cors());
+app.use(express.json())
 
 const users = [
     {id: 1, name: 'nahid inbe safi', email: 'nahid@gmail.com'},
@@ -12,25 +16,63 @@ const users = [
 
 
 
+// userName: dbUser1
+// password: KOjTpnMc6qzWclSA
+
+const uri = "mongodb+srv://dbUser1:KOjTpnMc6qzWclSA@cluster0.og8pjeq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        const userCollection = client.db('simpleNone').collection('users');
+
+        app.get('/users', async (req, res)=>{
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users)
+        });
+
+        app.post('/users', async (req, res)=>{
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            console.log(result)
+            user._id = result.insertedId;
+            res.send(user);
+
+        })
+    }
+    finally{
+
+    }
+}
+run().catch((err)=> console.log(err));
+
+
+
 app.get('/', (res, req)=>{
     req.send("simple node server running");
 });
 
 
-app.use(cors());
-app.use(express.json())
 
-app.get('/users', (req, res)=>{
-    res.send(users);
-});
+// app.get('/users', (req, res)=>{
+//     if(req.query.name){
+//         const search = req.query.name;
+//         const filtered = users.filter(usr => usr.name.toLocaleLowerCase().indexOf(search) >= 0)
+//         res.send(filtered);
+//     }
+//     else{
+//         res.send(users);
+//     }
+// });
 
-app.post('/users', (req, res)=>{
-    // console.log('api called');
-    const user = req.body;
-    user.id = users.length + 1;
-    users.push(user)
-    res.send(user);
-})
+// app.post('/users', (req, res)=>{
+//     // console.log('api called');
+//     const user = req.body;
+//     user.id = users.length + 1;
+//     users.push(user)
+//     res.send(user);
+// })
 
 app.listen(port, (res, req)=>{
     console.log(`simple node server running on port ${port}`);
